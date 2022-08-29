aea62269bdbd468b6f342b805740358616a83d04
Peb Yog Hmong <pyhteam.solutions@gmail.com>
2022-08-29 11:26:32 +0700

    delete bin

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,61 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<UsersController> _logger;
+        public UsersController(DataContext context, ILogger<UsersController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        // GET: api/<UsersController>
+        [HttpGet]
+        [Route("get-all")]
+        public async Task<IEnumerable<AppUser>> Get()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet]
+        [Route("get/{id}")]
+        public async Task<AppUser> Get(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+            {
+                _logger.LogInformation($"User with id {id} not found.");
+                return null;
+            }
+            return user;
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
